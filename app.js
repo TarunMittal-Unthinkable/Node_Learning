25d2f98fb95ce887640edfc07b11179e452e685d
TarunMittal-Unthinkable <tarun.mittal@unthinkable.co>
2022-04-28 17:13:10 +0530

    Third commit

--- app.js
+++ app.js
@@ -33,3 +33,20 @@ console.log("The arrow function way-2: ",res3);
 
 // const res4=help.min(20,4);
 // console.log("The export way-2: ",res4);
+
+
+//nodemon test
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    res.end("Hello from Tarun Mittal and Unthinkable Solution");
+});
+// server.listen(3000);
+
+//Express Test
+
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("Hey! Whatsup")
+});
+app.listen(3000);
