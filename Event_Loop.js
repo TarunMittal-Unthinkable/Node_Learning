59da05a9787a1d31c80dbd8b2465b41a2db5b76f
TarunMittal-Unthinkable <tarun.mittal@unthinkable.co>
2022-04-28 18:49:00 +0530

    Async and Sync Demo

--- Event_Loop.js
+++ Event_Loop.js
@@ -1,5 +1,31 @@
 const fs=require('fs');
 const fileName="target.txt";
-fs.watch(fileName,()=>
-    console.log(`File changed!`));
 
+// fs.watch(fileName,()=>
+//     console.log(`File changed!`));
+
+// //Sync Program demo
+// const data=fs.readFileSync(fileName);
+// console.log("Sync Output ",data.toString());
+
+
+
+//Functional Approach
+const errHandler=err=>console.log(err);
+const dataHandler=data=>console.log("Functional Approach: ",data.toString());
+fs.readFile(fileName,(err,data)=>{
+    if(err) errHandler(err);
+    dataHandler(data);
+    });
+
+
+//Async Program demo
+fs.readFile(fileName,(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log("Async Output ",data.toString());
+    
+});
+
+console.log('Node js async-sync Programming');
