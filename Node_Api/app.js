fb90df26ac8576c19a5c02e75486ee4df771ee2d
TarunMittal-Unthinkable <tarun.mittal@unthinkable.co>
2022-04-29 15:20:46 +0530

    Separting Route

--- Node_Api/app.js
+++ Node_Api/app.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const app=express();
-app.get('/',(req,res)=>{
-    res.send("Hey! Whatsup India")
-});
+// app.get('/',(req,res)=>{
+//     res.send("Hey! Whatsup India")
+// });
+
+
+//bring in routes
+
+const postRoutes=require('./routes/post');
+app.get('/',postRoutes.getPosts);
+
 const port=8000;
 app.listen(port, ()=>{
     console.log(`A Node Js API is listening on port: ${port}`);
---
+++ Node_Api/routes/post.js
@@ -0,0 +1,9 @@
+
+
+const getPosts=(req,res)=>{
+    res.send("Hey! Whatsup India helloji kemcho")
+}
+
+module.exports={
+    getPosts
+}
