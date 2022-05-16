32bba085d5b0a451df913ac70fc00f64d3632f47
TarunMittal-Unthinkable <tarun.mittal@unthinkable.co>
2022-05-16 17:20:34 +0530

    SequelizePractice

---
+++ Sequalize_Prac/config/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports =  new Sequelize(process.env.DATABASE_URL, {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+});
